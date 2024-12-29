2116f172064d0e6734d5f230b8f42a27e4fcb1a2
Shrihari Chaurasia <126378618+shrihari143@users.noreply.github.com>
2024-12-29 11:46:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,37 +5,36 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const path = require("path");
 const connectDb = require("./Config/connectDb");
-// config dot env file
+
+// Config dot env file
 dotenv.config();
 
-//databse call
+// Database call
 connectDb();
 
-//rest object
+// Initialize express app
 const app = express();
 
-//middlewares
+// Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-//routes
-//user routes
-app.use("/api/v1/users", require("./routes/userRoutes"));
-//transections routes
-app.use("/api/v1/transections", require("./routes/taskRoutes"));
+// Load routes
+require("./routes")(app);
 
-//static files
+// Serve static files
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Fallback route
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
+);
 
-//port
-const PORT = 8000 || process.env.PORT;
+// Port
+const PORT = process.env.PORT || 8000;
 
-//listen server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Start server
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`.cyan.bold)
+);
